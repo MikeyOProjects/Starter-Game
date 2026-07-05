fd30870e09f970f551b58c4d9426a897c90b31d7
Mikey O <themiku13@gmail.com>
2026-07-05 16:22:35 -0500

    Setting up the basics

--- Beginner/beginner.py
+++ Beginner/beginner.py
@@ -1,1 +1,12 @@
-print("hello world")
+Max_Health = 100
+current_health = 100
+
+weapon = "Sword"
+shield = "iron shield"
+
+items = ["Minor Potion", "Potion", "Max Potion"]
+
+Minor_potion = 20
+Potion = 50
+Max_Potion = 100
+
